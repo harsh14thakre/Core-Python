dff9643d78cf54ff831718bbd67849fe007b0052
harsh14thakre <harsh14thakre@gmail.com>
2025-05-05 23:08:51 +0530

    commit

---
+++ day 14 control statements/control_statements.py
@@ -0,0 +1,91 @@
+#  CONTROL STATEMENT IS  DIVIDED INTO THREE PARTS 
+# 1 Conditional statement    2 Iterative statement          3 transfer statement 
+    # |-if                        |-while loop                    |-continue
+    # |-if else                   |-for loop                      |-pass
+    # |-if elif                                                   |-break
+    # |-if elif else
+
+
+
+
+# x=10
+# y=20
+
+# if y>x:
+#     print("y is greater than x")
+
+
+
+
+
+# x=int(input("Enter any number"))
+
+# if(x%2==0):
+#     print(f'given number is {x} is even')
+# print
+
+# x=int(input("Enter first number"))
+# y=int(input("Enter second number"))
+# z=x+y
+# # print(x+y)
+# print(f'sum of  {x} + {y}={z}')
+
+
+# x=int(input("Enter first number"))
+# y=int(input("Enter second number"))
+# print(f'x= {y} \n y={x}' )
+
+# x,y=y,x
+# print(f'x={x} \n y={y}' )
+
+# temp=y
+# y=x
+# x=temp
+# print(f'x={x} \n y={y}' )
+
+
+#---------------------------------------------------------------Q=4-------------------------------------------
+
+
+# x=int(input("Enter any number"))
+
+# print(type(x))
+# print(id(x))
+
+
+
+
+
+#Q1=>Swap two numbers without using third variable
+
+# x=int(input("Enter any 1st number:-"))
+# y=int(input("Enter any 2nd number:-"))
+
+# x,y=y,x
+# print(f'x={x} \n y={y}')
+
+#Q2=>Swap two numbers using third variable
+
+# x=int(input("Enter any 1st number:-"))
+# y=int(input("Enter any 2nd number:-"))
+
+# temp=x
+# x=y
+# y=temp
+# print(f'x={x} \n y={y}')
+
+#Q3=>Swap two numbers using addition/subtraction/multiplication and division.
+
+# x=int(input("Enter any 1st number:-"))
+# y=int(input("Enter any 2nd number:-"))
+
+
+#Q4=>Write a program to take input from runtime and print id and type of that input.
+
+# x=int(input("Enter any number"))
+
+# print(type(x))
+# print(id(x))
+
+
+#Q5=>
