32db5e04aff93adea86d8bc9ab975d34ab7765bf
harsh14thakre <harsh14thakre@gmail.com>
2025-05-11 23:25:10 +0530

    commit

---
+++ decorators.py
@@ -0,0 +1,23 @@
+ ######### decorator is used to modify and change the behaviour of other function without changing its code
+
+# def decorator(fun): 
+
+#     return 5
+
+# def main_fun():
+#     print("main function")
+# x=decorator(main_fun)
+# # x()
+# print(x)
+
+
+# def decor():
+#     def inner():
+#         print("hello")
+#     return inner
+
+# x=decor()
+# print(x)
+# x()
+
+            ###############################
