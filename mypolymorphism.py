d31b1524bdf07266082f71f77401407f2b88be98
harsh14thakre <harsh14thakre@gmail.com>
2025-05-14 18:04:05 +0530

    commit

---
+++ mypolymorphism.py
@@ -0,0 +1,12 @@
+####################################Polymorphism##########################################
+
+class A:
+    def add(self,*n):
+        sum=0
+        for i in n:
+            sum=sum+i
+        print(sum) 
+obj=A()
+obj.add()
+obj.add(1)
+obj.add(1,2,3,4,5)        
